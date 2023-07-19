27c611c271082f3ec2ae81c3b6b216417d525e55
DarioEsposito89 <darioesposito.work@gmail.com>
2023-07-19 10:02:02 +0200

    messo 0 nell'Href per non tornare all'inizio

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ function stampaPost(item, index) {
         <div class="post__footer">
             <div class="likes js-likes">
                 <div class="likes__cta">
-                    <a class="like-button  js-like-button" href="#" data-postid="${posts[index].id}" id="button${[index + 1]}">
+                    <a class="like-button  js-like-button" href="#0" data-postid="${posts[index].id}" id="button${[index + 1]}">
                         <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                         <span class="like-button__label">Mi Piace</span>
                     </a>
